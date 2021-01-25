4fff6850fc6d948a8551d0ce9675cc4d552478a2
h1m <himanshu.yadav643@gmail.com>
2021-01-26 05:29:37 +0530

    created UserBusinessService and UserAdminController

--- proman-api/src/main/java/com/upgrad/proman/api/controller/UserAdminController.java
+++ proman-api/src/main/java/com/upgrad/proman/api/controller/UserAdminController.java
@@ -2,7 +2,11 @@ package com.upgrad.proman.api.controller;
 
 import com.upgrad.proman.api.model.UserDetailsResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/")
 public class UserAdminController {
 
     public ResponseEntity<UserDetailsResponse> getUser(final String userUuid) {
