ceb667e6c1ba1b8f55a30be941a4a046886d8357
h1m <himanshu.yadav643@gmail.com>
2021-01-26 05:05:15 +0530

    starting getUser endpoint

---
+++ proman-api/src/main/java/com/upgrad/proman/api/controller/UserAdminController.java
@@ -0,0 +1,11 @@
+package com.upgrad.proman.api.controller;
+
+import com.upgrad.proman.api.model.UserDetailsResponse;
+import org.springframework.http.ResponseEntity;
+
+public class UserAdminController {
+
+    public ResponseEntity<UserDetailsResponse> getUser(final String userUuid) {
+
+    }
+}
